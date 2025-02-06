fc477051436bc6938477a28fea0714ee12c5465b
neverleaveualong <wmr06244@naver.com>
2025-02-06 17:10:52 +0900

    Final Modified

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const main_view = fs.readFileSync('./../main.html', 'utf-8');
+const buy_view = fs.readFileSync('./../buy.html', 'utf-8');
+const order_view = fs.readFileSync('./../orderlist.html','utf-8');
+
 const mariadb = require('./../database/connect/mariadb');
+
+const main_css = fs.readFileSync('./../css/main.css', 'utf-8');
+const orderlist_css = fs.readFileSync('./../css/orderlist.css', 'utf-8');
 
 function main(response){
     console.log('main');
@@ -45,21 +51,41 @@ function order(response, productId) {
         console.log(rows);
     })
 
-    response.write('order page');
+    response.write(buy_view);
     response.end();
 }
 
 function orderlist(response){
-    fs.readFile('./../orderlist.html', function(err,data) {
-        response.writeHead(200, {'Content-Type' : 'text/html'});
-        response.write(data);
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    mariadb.query("SELECT * FROM orderlist", function(err,rows){
+        console.log(rows);
+        response.write(order_view);
+        rows.map(item=>{
+            response.write("<tr>"
+                           +"<td>"+item.product_id+"</td>"
+                           +"<td>"+item.order_data+"</td>"
+                           +"</tr>");
+                        });
+        response.write("</table>");
         response.end();
-        });
+    })
 }
 
 function mainhtml(response){
     response.writeHead(200, {'Content-Type' : 'text/html'});
     response.write(main_view);
+    response.end();
+}
+
+function maincss(response) {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.write(main_css);
+    response.end();
+}
+
+function orderlistcss(response) {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.write(orderlist_css);
     response.end();
 }
 
@@ -74,5 +100,8 @@ handle['/img/blackRacket.png'] = blackRacket;
 
 handle['/orderlist.html'] = orderlist;
 handle['/main.html'] = mainhtml;
+
+handle['/css/orderlist.css'] = orderlistcss;
+handle['/css/main.css'] = maincss;
 
 exports.handle = handle;
